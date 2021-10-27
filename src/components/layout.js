45084c64bad05730b59b42ecbab9e2436a67064d
zune <1119548217@qq.com>
2021-10-27 12:34:24 +0800

    增加移动端导航菜单

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,8 @@
 import * as React from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
 
-const Layout = ({ location, title, children, to='/' }) => {
+const Layout = ({ location, title, children, to = '/' }) => {
+  const [isMenuExtended, setIsMenuExtended] = React.useState(false);
   const data = useStaticQuery(graphql`
     query LayoutQuery {
       site {
@@ -16,6 +17,8 @@ const Layout = ({ location, title, children, to='/' }) => {
     }
   `)
 
+  const handleMenuBtnClick = () => setIsMenuExtended(isMenuExtended => !isMenuExtended)
+
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
   let header
@@ -23,33 +26,33 @@ const Layout = ({ location, title, children, to='/' }) => {
   const menus = data.site.siteMetadata?.menus
   const description = data.site.siteMetadata?.description
 
-  if (isRootPath) {
-    header = (
-      <>
-        <div className="heading-container">
-          <h1 className="main-heading">
-            <Link to={to}>{title}</Link>
-          </h1>
-          <nav className="nav flex justify-between">
+  header = (
+    <>
+      <div className="global-header-mask" onClick={()=>setIsMenuExtended(false)}/>
+      <div className="heading-container">
+        <h1 className={`main-heading ${isRootPath ? '' : 'main-heading-other-path'}`}>
+          <Link to={to}>{title}</Link>
+        </h1>
+        <button className="global-menu-btn" onClick={handleMenuBtnClick} title="open the nav menu">
+          <div className="hamburger-btn">
+            <div className="hamburger-btn-line" />
+          </div>
+        </button>
+        <nav className="global-nav flex justify-between">
+          <ul className="global-nav-list">
             {
               menus.map((item, idx) => <li key={idx} className="nav-item"><Link className="nav-item-link" to={item.slug}>{item.name}</Link></li>)
             }
-          </nav>
-        </div>
-      </>
-    )
-  } else {
-    header = (
-      <Link className="header-link-home" to={to}>
-        {title}
-      </Link>
-    )
-  }
+          </ul>
+        </nav>
+      </div>
+    </>
+  )
 
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
-      <header className="global-header">{header}</header>
-      <main>{children}</main>
+      <header className="global-header" aria-expanded={isMenuExtended}>{header}</header>
+      <main className="main-wrapper">{children}</main>
       <footer>
         © {new Date().getFullYear()}, {description}
       </footer>
