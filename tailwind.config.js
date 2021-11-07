22942914c7644acf0f0d883d357abab6ee5582ac
zune <1119548217@qq.com>
2021-11-07 12:50:17 +0800

    删除行内代码前后的符号

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            'code:not(div)': {
+              '&::after, &::before': {
+                content: '""'
+              }
+            }
+          }
+        }
+      }),
       textColor: {
         'primary': '#005b99',
         'secondary': '#2e353f',
