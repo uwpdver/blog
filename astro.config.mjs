b88a5f0e443e3e6a15faa47bc2d039d9511e3e5a
zhihao <liuzhihao@lagride.com.ng>
2025-04-03 11:25:43 +0800

    fix: 修复 astro.config.mjs 中的 base 路径格式

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: "https://uwpdver.github.io",
-  base: "/blog-2025"
+  base: "/blog-2025",
   integrations: [mdx(), sitemap(), tailwind()],
 });
