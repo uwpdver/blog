e6be2485ecfc680b142d3d7d692c54aff3be31c7
timeisrunningout <1119548217@qq.com>
2025-04-03 11:23:03 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: "https://uwpdver.github.io",
+  base: "/blog-2025"
   integrations: [mdx(), sitemap(), tailwind()],
 });
