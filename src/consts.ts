364a541485ef18b3588a1678f6b4bc8a381209d2
zhihao <liuzhihao@lagride.com.ng>
2025-04-03 19:03:31 +0800

    feat: 添加网站图标和logo，更新头部组件以使用新logo

--- src/consts.ts
+++ src/consts.ts
@@ -6,6 +6,7 @@ export const SITE: Site = {
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
+  LOGO: "/blog/logo.png",
 };
 
 export enum PAGE_KEY {
--- src/types.ts
+++ src/types.ts
@@ -4,6 +4,7 @@ export type Site = {
   NUM_POSTS_ON_HOMEPAGE: number;
   NUM_WORKS_ON_HOMEPAGE: number;
   NUM_PROJECTS_ON_HOMEPAGE: number;
+  LOGO: string;
 };
 
 export type Metadata = {
